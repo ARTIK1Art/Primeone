646e5b39fc412ec1967659634ad7846d106306ed
ARTIK1Art <98806951+ARTIK1Art@users.noreply.github.com>
2022-11-03 21:08:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+document.querySelector('.header__burger').addEventListener('click', () => {
+   document.querySelector('.header__burger').classList.toggle('active');
+ });
+ document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__menu').classList.toggle('active');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.body').classList.toggle('lock');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__logo').classList.toggle('active');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__online').classList.toggle('active');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__nav-02').classList.toggle('active');
+});
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.header__column-01').classList.toggle('active');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.menu-header__list').classList.toggle('active');
+});
+document.querySelector('.header__burger').addEventListener('click', () => {
+  document.querySelector('.menu-header__listtwo').classList.toggle('active');
+});
+
+
+
